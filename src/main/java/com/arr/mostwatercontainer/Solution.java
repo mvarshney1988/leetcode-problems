924a86386323e46a33e5d35098a74e1d8ab903cf
mvarshney <mvarshney@ea.com>
2021-10-30 21:05:14 +0530

    pom

---
+++ src/main/java/com/arr/mostwatercontainer/Solution.java
@@ -0,0 +1,21 @@
+package com.arr.mostwatercontainer;
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int sum=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                sum=Math.max(sum,height[left]*(right-left));
+                left++;
+            }else{
+                sum=Math.max(sum,height[right]*(right-left));
+                right--;
+            }
+
+
+        }
+        return sum;
+    }
+}
