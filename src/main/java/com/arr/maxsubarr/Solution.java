16f426373873a71d63e5557fa4a459177c2d1499
mvarshney <mvarshney@ea.com>
2021-10-31 08:46:27 +0530

    array and math problems

--- src/main/java/com/arr/maxsubarr/Solution.java
+++ src/main/java/com/arr/maxsubarr/Solution.java
@@ -1,4 +1,24 @@
 package com.arr.maxsubarr;
 
 public class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        int sum=nums[0];
+        int sumsofar=0;
+        for (int i=0;i<nums.length;i++){
+            sumsofar+=nums[i];
+            sumsofar=Math.max(nums[i],sumsofar);
+
+            if(sumsofar>sum){
+                sum=sumsofar;
+            }
+
+
+
+        }
+        return sum;
+    }
+
 }
