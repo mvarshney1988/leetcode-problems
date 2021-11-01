26d3cce7a721395704a3dc320e5f6a7196cade52
mvarshney <mvarshney@ea.com>
2021-11-01 11:32:52 +0530

    reverse string2

---
+++ src/main/java/com/str/reverse2/Solution.java
@@ -0,0 +1,24 @@
+package com.str.reverse2;
+
+public class Solution {
+    public String reverseStr(String s, int k) {
+        if(k==1)
+            return s;
+        char[] ch = s.toCharArray();
+        for(int i=0;i<s.length();i+=2*k){
+            int start=i;
+            int end=Math.min(i+k-1,s.length()-1);
+            while(start<end){
+                char temp=ch[start];
+                ch[start]=ch[end];
+                ch[end]=temp;
+                start++;
+                end--;
+
+            }
+        }
+        return String.valueOf(ch);
+    }
+
+
+}
